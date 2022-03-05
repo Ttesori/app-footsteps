a0a6a75152b5c6180b494d744db0a78f90cad968
Toni Tesori <tt@tesoridesign.com>
2022-03-05 16:40:32 -0500

    Add cors

--- back/server.js
+++ back/server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
 
+// CORS
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
 // Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/api/hikes', require('./routes/hikeRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/hikes', require('./routes/hikeRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
-})
+});
