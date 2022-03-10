7f09a1dff5a40bfca2f6c47f019f3733cd658fb5
Toni Tesori <tt@tesoridesign.com>
2022-03-10 11:11:32 -0500

    Begin layout work

--- front/src/layouts/MainLayout.js
+++ front/src/layouts/MainLayout.js
@@ -1,12 +1,24 @@
+import '../css/Layout.css';
+import { useState } from 'react';
+
 const MainLayout = ({ children }) => {
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <>
-      <header>
+      <header className='header'>
         <div className="container">
-          <h1>Footsteps</h1>
+          <h1 className="header__logo">Footsteps</h1>
         </div>
       </header>
-      <main>
+      <nav className={`nav ${navOpen ? 'nav--open' : 'nav--closed'}`}>
+        <button className="nav__btn" onClick={() => setNavOpen(!navOpen)}>Menu</button>
+        <menu className={`nav__list`}>
+          <li className="nav__list-item">
+            <a href="#" className="nav__list-link">Log Out</a>
+          </li>
+        </menu>
+      </nav>
+      <main className="main">
         {children}
       </main>
       <footer>
