1399519529c88d1f1c38613ce089c9d030be8481
Toni Tesori <tt@tonitesori.dev>
2022-06-01 15:06:55 -0400

    Styling progress

--- front/src/components/HikeItem.js
+++ front/src/components/HikeItem.js
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import DataContext from "../context/DataContext";
 import '../css/HikeItem.css';
+import { IoLocationSharp } from "react-icons/io5";
+import { FaClock, FaEdit } from "react-icons/fa";
 
 const HikeItem = ({ hike }) => {
   const { hikes, setHikeToEdit, setCreateIsOpen } = useContext(DataContext);
@@ -25,14 +27,21 @@ const HikeItem = ({ hike }) => {
       <div className="hike-item__title-group">
         <h3 className="hike-item__title">{hike.title}</h3>
         <ul className="hike-item__details">
-          {hike.date && <li className="hike-details__date">{formatDate(hike.date)}</li>}
-          <li className="hike-details__location">{hike.location}</li>
+          {hike.date &&
+            <li className="hike-details__date">
+              <FaClock />
+              {formatDate(hike.date)}
+            </li>}
+          <li className="hike-details__location">
+            <IoLocationSharp />
+            {hike.location}
+          </li>
         </ul>
       </div>
 
       {hike.distance && <div className="hike-item__distance">{hike.distance}<span className="hike-distance__unit">mi</span></div>}
       <p className="hike-item__notes">{hike.notes}</p>
-      <button onClick={() => handleEditHike(hike._id)} className="btn btn--hike-edit">Edit</button>
+      <button onClick={() => handleEditHike(hike._id)} className="btn btn--hike-edit"><FaEdit /> <span className="sr-only">Edit</span></button>
     </section>
   );
 };
