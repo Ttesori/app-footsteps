9d38743045289ccae5c4156c3f4ac01776c85970
Toni Tesori <tt@tonitesori.dev>
2022-07-28 12:03:58 -0400

    Continue test and refine

--- back/server.js
+++ back/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: ['http://localhost:3000', 'https://footsteps-app.herokuapp.com/']
 }));
 
 // Body parser
