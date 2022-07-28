5f7fecb0a1a7e29d685f85fb2d82d08841c39f9b
Toni Tesori <tt@tonitesori.dev>
2022-07-28 12:07:41 -0400

    Add to cors

--- back/server.js
+++ back/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://footsteps-app.herokuapp.com/']
+  origin: ['http://localhost:3000', 'https://footsteps-app.herokuapp.com/', 'https://dreamy-selkie-bbda31.netlify.app/']
 }));
 
 // Body parser
